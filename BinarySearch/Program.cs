7dc96cd60a3fb98a14bfcd2569f8b5bf56ebb17f
dejanradulov992 <54819882+dejanradulov992@users.noreply.github.com>
2019-09-02 22:58:55 +0200

    Add files via upload

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 0, 1, 10, 23, 34, 45, 67, };
+            Console.WriteLine(BinarySearchIterative(arr1, 34));
+            
+
+            Console.WriteLine(BinarySearchRecursive(arr1, 34, 0 , arr1.Length-1));
+            Console.ReadLine();
+        }
+
+        public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            if (min > max)
+            {
+                return "Number does not exist!!!";
+            }
+            else
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return "Number is on position number " + ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    return BinarySearchRecursive(inputArray, key, min, mid - 1);
+                }
+                else
+                {
+                    return BinarySearchRecursive(inputArray, key, mid + 1, max);
+                }
+            }
+        }
+
+        public static object BinarySearchIterative(int[] inputArray, int key)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return "Number is on position number " + ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return "Number does not exist!!!";
+        }
+    }
+}
